6d18f641db3c212fc5f823573120754ce29631e7
Maatteogekko <matteogent9991@gmail.com>
2025-01-09 22:38:13 +0100

    add basic flake for nix support

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nix flake for Mullvad's wgephemeralpeer";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
+    packages.x86_64-linux.wgephemeralpeer = pkgs.buildGoModule {
+      name = "wgephemeralpeer";
+      version = "1.0.5"; # Replace with the desired version
+
+      src = ./.; 
+      vendorHash = null;
+
+      subPackages = [ "./cmd/mullvad-upgrade-tunnel" ];
+
+      meta = with pkgs.lib; {
+        description = "Mullvad's post-quantum-secure WireGuard tunnels tool";
+        license = licenses.gpl3;
+        maintainers = [ maintainers.maatteogekko ];
+        platforms = platforms.linux;
+      };
+    };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.wgephemeralpeer;
+  };
+}
